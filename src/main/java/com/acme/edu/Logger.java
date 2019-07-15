81a93751db846518df94c3a087233f102abcb51e
PosAlina <pospelowa.alina@yandex.ru>
2019-07-15 16:13:43 +0300

    Logger: improve flexibility, added new methods to decorate and get type of object

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,26 +1,32 @@
 package com.acme.edu;
 
 public class Logger {
+    private static String getMessageType(Object message) {
+        String typeName;
+        if (message instanceof String) {
+            typeName = "string";
+        } else if (message instanceof Integer || message instanceof Byte || message instanceof Boolean) {
+            typeName = "primitive";
+        } else if (message instanceof Character) {
+            typeName = "char";
+        } else {
+            typeName = "reference";
+        }
+        return typeName;
+    }
+
+    private static String decorate(Object message) {
+        if (message == null) {
+            return "null";
+        }
+        return getMessageType(message) + ": " + message;
+    }
 
     private static void printToConsole(String message) {
         System.out.println(message);
     }
 
-    public static void log(char message) {
-        printToConsole("char: " + message);
-    }
-
     public static void log(Object message) {
-        String typeName;
-        if (message instanceof String) {
-            typeName = "string: ";
-        } else if (message instanceof Integer || message instanceof Byte || message instanceof Boolean) {
-            typeName = "primitive: ";
-        } else if (message == null) {
-            typeName = "null: ";
-        } else {
-            typeName = "reference: ";
-        }
-        printToConsole(typeName + message);
+        printToConsole(decorate(message));
     }
 }
