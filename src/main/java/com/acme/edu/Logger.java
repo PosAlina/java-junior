b3cdb2c1a309aae48f52e527510f7bba291d465e
PosAlina <pospelowa.alina@yandex.ru>
2019-07-15 17:05:38 +0300

    Logger class:
    Improved design, string constants for prefixes added

--- src/main/java/com/acme/edu/Logger.java
+++ src/main/java/com/acme/edu/Logger.java
@@ -1,32 +1,43 @@
 package com.acme.edu;
 
 public class Logger {
-    private static String getMessageType(Object message) {
-        String typeName;
-        if (message instanceof String) {
-            typeName = "string";
-        } else if (message instanceof Integer || message instanceof Byte || message instanceof Boolean) {
-            typeName = "primitive";
-        } else if (message instanceof Character) {
-            typeName = "char";
-        } else {
-            typeName = "reference";
-        }
-        return typeName;
-    }
+    private static final String STRING_PREFIX = "string";
+    private static final String PRIMITIVE_PREFIX = "primitive";
+    private static final String CHAR_PREFIX = "char";
+    private static final String REFERENCE_PREFIX = "reference";
 
-    private static String decorate(Object message) {
+    private static String decorate(String prefix, Object message) {
         if (message == null) {
             return "null";
         }
-        return getMessageType(message) + ": " + message;
+        return prefix + ": " + message;
     }
 
-    private static void printToConsole(String message) {
+    private static void save(String message) {
         System.out.println(message);
     }
 
+    public static void log(String message) {
+        save(decorate(STRING_PREFIX, message));
+    }
+
+    public static void log(byte message) {
+        save(decorate(PRIMITIVE_PREFIX, message));
+    }
+
+    public static void log(int message) {
+        save(decorate(PRIMITIVE_PREFIX, message));
+    }
+
+    public static void log(boolean message) {
+        save(decorate(PRIMITIVE_PREFIX, message));
+    }
+
+    public static void log(char message) {
+        save(decorate(CHAR_PREFIX, message));
+    }
+
     public static void log(Object message) {
-        printToConsole(decorate(message));
+        save(decorate(REFERENCE_PREFIX, message));
     }
 }
